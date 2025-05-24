700b6bf90096d57db3633dfbc4e0f22874226d55
MiguelMods <miguelmodd@gmail.com>
2025-05-24 19:22:53 -0400

    agreando repositorio generico

---
+++ Expensive.Application/Repository/Contract/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Expensive.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Expensive.Application.Repository.Contract;
+
+public interface IGenericRepository<Type> where Type : BaseEntity
+{
+    Task<IEnumerable<Type?>> GetAllAsync();
+    Task<Type?> GetByRowGuidAsync(string rowGuid);
+    Task<Type?> GetByExpressionAsync(Expression<Func<Type, bool>> expression);
+    Task<Type?> AddAsync(Type entity);
+    Task<Type?> UpdateAsync(Type entity);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ Expensive.Persistance/Repository/Implementations/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Expensive.Application.Repository.Contract;
+using Expensive.Domain.Entities;
+using Expensive.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Expensive.Persistance.Repository.Implementations;
+
+public class GenericRepository<Type>(ExpensiveApplicationDataContext expensiveApplicationDataContext) : IGenericRepository<Type> where Type : BaseEntity
+{
+    public ExpensiveApplicationDataContext ExpensiveApplicationDataContext { get; } = expensiveApplicationDataContext;
+
+    public async Task<IEnumerable<Type?>> GetAllAsync()
+        => await ExpensiveApplicationDataContext.Set<Type>().ToListAsync();
+    public async Task<Type?> GetByRowGuidAsync(string rowGuid)
+        => await ExpensiveApplicationDataContext.Set<Type>().FirstOrDefaultAsync(x => x.RowGuid == rowGuid);
+    public async Task<Type?> GetByExpressionAsync(Expression<Func<Type, bool>> expression)
+        => await ExpensiveApplicationDataContext.Set<Type>().FirstOrDefaultAsync(expression);
+    public Task<Type?> AddAsync(Type entity)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<Type?> UpdateAsync(Type entity)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
