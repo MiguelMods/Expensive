6c5b016149597042cf4cef74d54e2ba3c36e49a8
MiguelMods <miguelmodd@gmail.com>
2025-05-24 18:43:58 -0400

    agregando contexto de datos entity framework

---
+++ Expensive.Persistance/Context/ExpensiveApplicationDataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expensive.Persistance.Context;
+
+public class ExpensiveApplicationDataContext(DbContextOptions<ExpensiveApplicationDataContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbContextOptions DbContextOptions { get; } = dbContextOptions;
+}
