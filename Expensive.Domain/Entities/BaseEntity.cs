872793109436c26ea8cc1d4f792e25c6f4c1176c
MiguelMods <miguelmodd@gmail.com>
2025-05-24 18:40:49 -0400

    Add project files.

---
+++ Expensive.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Expensive.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public string? RowGuid { get; set; }
+    }
+}
---
+++ Expensive.Domain/Entities/Categories.cs
@@ -0,0 +1,8 @@
+﻿namespace Expensive.Domain.Entities;
+
+public class Categories : BaseEntity
+{
+    public int CategorieId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
