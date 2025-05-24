43c08b8fe20b9c4e6dfcbd8f2f8e88436fc97141
MiguelMods <miguelmodd@gmail.com>
2025-05-24 19:11:11 -0400

    agregando nuevas migraciones

---
+++ Expensive.Persistance/Dependencies.cs
@@ -0,0 +1,15 @@
+﻿using Expensive.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Expensive.Persistance;
+
+public static class Dependencies
+{
+    public static void AddPersistance(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ExpensiveApplicationDataContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ExpansiveApiDatabaseConnection")));
+    }   
+}
---
+++ Expensive.Persistance/Migrations/20250524230948_AddCategories.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expensive.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "dbo",
+                columns: table => new
+                {
+                    CategorieId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    RowGuid = table.Column<string>(type: "nvarchar(max)", nullable: true, defaultValue: "NEWID()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategorieId);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Categories",
+                columns: new[] { "CategorieId", "CreatedBy", "Description", "Name", "UpdatedBy" },
+                values: new object[] { 1, "Created By Default", "Default category", "Default", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                schema: "dbo",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "dbo");
+        }
+    }
+}
