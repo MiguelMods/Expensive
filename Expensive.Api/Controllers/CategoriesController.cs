df2e6ca2cdd88a1d1a55df731f2bb54ffec0dc32
MiguelMods <miguelmodd@gmail.com>
2025-05-24 20:00:06 -0400

    removiendo controlador no necesario y agregando nuevo controlador

---
+++ Expensive.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using Expensive.Application.Repository.Contract;
+using Expensive.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace Expensive.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController(IUnitOfWork unitOfWork) : ControllerBase
+    {
+        private IUnitOfWork UnitOfWork { get; } = unitOfWork;
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await UnitOfWork.Categories.GetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{rowguid}")]
+        public async Task<IActionResult> Get(string rowguid)
+        {
+            var categories = await UnitOfWork.Categories.GetByRowGuidAsync(rowguid);
+            return Ok(categories);
+        }
+
+        [HttpGet("{parameterName}/{parameterValue}")]
+        public async Task<IActionResult> Get(string parameterName, string parameterValue)
+        {
+            Expression<Func<Categories, bool>> expression = parameterName switch
+            {
+                "categoriesId" => (x => x.CategorieId == long.Parse(parameterValue)),
+                "Description" => x => x.Description == parameterValue,
+                _ => throw new ArgumentException("Invalid parameter name")
+            };
+            var categories = await UnitOfWork.Categories.GetByExpressionAsync(expression);
+            return Ok(categories);
+        }
+    }
+}
