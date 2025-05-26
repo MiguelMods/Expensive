91c1cf94660fdd738398f3af658511894f73f41f
MiguelMods <miguelmodd@gmail.com>
2025-05-25 20:14:35 -0400

    agregando nuevos elementos a la clase dataContext

--- Expensive.Persistance/Context/ExpensiveApplicationDataContext.cs
+++ Expensive.Persistance/Context/ExpensiveApplicationDataContext.cs
@@ -7,8 +7,16 @@ namespace Expensive.Persistance.Context;
 public class ExpensiveApplicationDataContext(DbContextOptions<ExpensiveApplicationDataContext> dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<Categories> Categories { get; set; }
+    public DbSet<Users> Users { get; set; }
+    public DbSet<UserCategories> UserCategories { get; set; }
+    public DbSet<UserPaymentMethods> UserPaymentMethods { get; set; }
+    public DbSet<PaymentMethods> PaymentMethods { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new CategoriesEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new UserCategoriesEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new PaymentMethodsEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new UserPaymentMethodsEntityConfiguration());
     }
 }
