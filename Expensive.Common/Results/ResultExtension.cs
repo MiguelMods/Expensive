aa61a6132ea37e2c4a5d766c83d5bf2340b1c610
Miguel Mata <mmata@altice.com.do>
2025-05-28 10:31:42 -0400

    agregando result, para manjear los resultados

---
+++ Expensive.Common/Results/ResultExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Expensive.Common.Results;
+
+public static class ResultExtension
+{
+    public static Result<Type> ToResult<Type>(this Type? data, string? message = null)
+    {
+        if (data is null)
+            return Result<Type>.Failure("Data is null");
+
+        return Result<Type>.Success(data, message);
+    }
+}
