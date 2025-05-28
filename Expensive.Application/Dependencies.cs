8af8ea1c3ed535e56be7ffd105510338e4526eeb
Miguel Mata <mmata@altice.com.do>
2025-05-28 10:31:23 -0400

    agregando servicios y otras funcionalidades

---
+++ Expensive.Application/Dependencies.cs
@@ -0,0 +1,15 @@
+﻿using Expensive.Application.Services.Contracts;
+using Expensive.Application.Services.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Expensive.Application;
+
+public static class Dependencies
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        // Register services
+        services.AddScoped<ICategorieService, CategorieService>();
+        services.AddScoped<IReadOnlyCategorieService, ReadOnlyCategorieService>();
+    }
+}
