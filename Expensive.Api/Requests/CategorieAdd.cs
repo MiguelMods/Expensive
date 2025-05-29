65be2cf15a4f8a99b41190b240d9456f6de94343
Miguel Mata <mmata@altice.com.do>
2025-05-29 08:34:53 -0400

    agregando nuevos request

--- Expensive.Api/Requests/CategorieAdd.cs
+++ Expensive.Api/Requests/CategorieAdd.cs
@@ -1,5 +1,4 @@
 ﻿namespace Expensive.Api.Requests
 {
     public record CategorieAdd(string Name, string Description, string Operation = "Expensive");
-    public record CategorieUpdate(string RowGuid, string Name, string Description, string Operation = "Expensive");
 }
---
+++ Expensive.Api/Requests/CategorieUpdate.cs
@@ -0,0 +1,4 @@
+﻿namespace Expensive.Api.Requests
+{
+    public record CategorieUpdate(string RowGuid, string Name, string Description, string Operation = "Expensive");
+}
