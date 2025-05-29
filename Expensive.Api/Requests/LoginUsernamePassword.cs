a40eec80eb327c900141c17f1633854f2e7ddac1
MiguelMods <miguelmodd@gmail.com>
2025-05-28 22:31:34 -0400

    agreganod nuevos modelos de usuario

---
+++ Expensive.Api/Requests/LoginUsernamePassword.cs
@@ -0,0 +1,5 @@
+﻿namespace Expensive.Api.Requests
+{
+    public record LoginRequest(string UserName, string Password);
+    public record LoginRegisterRequest(string FirtName, string LastName, string UserName, string Email, string Password);
+}
