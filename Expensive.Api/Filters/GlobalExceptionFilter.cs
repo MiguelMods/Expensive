e00100938bbd87cb1c98353aef2ddf07b0f79b9e
Miguel Mata <mmata@altice.com.do>
2025-05-30 11:12:42 -0400

    agregando filtros para validar modelos y capturar excepciones

---
+++ Expensive.Api/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Expensive.Common.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Expensive.Api.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            Result<Exception> result = Result<Exception>.Failure(exception.Message); ;
+
+            context.Result = new ObjectResult(result)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ Expensive.Api/Filters/RequestModelsValidatorsFilter.cs
@@ -0,0 +1,44 @@
+﻿using Expensive.Common.Results;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Expensive.Api.Filters
+{
+    public class RequestModelsValidatorsFilter(IServiceProvider serviceProvider) : IAsyncActionFilter
+    {
+        public IServiceProvider ServiceProvider { get; } = serviceProvider;
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            foreach (var argument in context.ActionArguments.Values)
+            {
+                if (argument == null)
+                {
+                    var result = Result<string[]>.Failure("Validation Errors");
+                    result.Data = ["Argument cannot be null"];
+                    context.Result = new BadRequestObjectResult(result);
+                    return;
+                }
+
+                var validatorType = typeof(IValidator<>).MakeGenericType(argument.GetType());
+
+                if (ServiceProvider.GetService(validatorType) is IValidator validator)
+                {
+                    var validationContext = new ValidationContext<object>(argument);
+                    var validationResult = await validator.ValidateAsync(validationContext);
+
+                    if (!validationResult.IsValid)
+                    {
+                        var result = Result<string[]>.Failure("Validation Errors");
+                        result.Data = validationResult.Errors.Select(e => e.ErrorMessage).ToArray();
+                        context.Result = new BadRequestObjectResult(result);
+                        return;
+                    }
+                }
+            }
+
+            await next();
+        }
+    }
+}
