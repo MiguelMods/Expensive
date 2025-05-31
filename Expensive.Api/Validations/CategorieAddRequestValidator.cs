0bc37fd1a1e15cd7d1988aab0d09372b5f57b637
MiguelMods <miguelmodd@gmail.com>
2025-05-31 14:12:00 -0400

    agregando validadores

---
+++ Expensive.Api/Validations/CategorieAddRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using Expensive.Api.Requests;
+using FluentValidation;
+
+namespace Expensive.Api.Validations;
+public class CategorieAddRequestValidator : AbstractValidator<CategorieAddRequest>
+{
+    public CategorieAddRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        RuleFor(x => x.Operation)
+            .NotEmpty().WithMessage("Operation is required.")
+            .MaximumLength(50).WithMessage("Operation must not exceed 50 characters.");
+    }
+}
---
+++ Expensive.Api/Validations/CategorieUpdateRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using Expensive.Api.Requests;
+using FluentValidation;
+
+namespace Expensive.Api.Validations;
+
+public class CategorieUpdateRequestValidator : AbstractValidator<CategorieUpdateRequest>
+{
+    public CategorieUpdateRequestValidator()
+    {
+        RuleFor(x => x.RowGuid).NotEmpty().WithMessage("RowGuid is required.");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+    }
+}
