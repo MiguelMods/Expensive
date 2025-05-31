101d9c63253355c809445b57fad88cefedfd22b2
MiguelMods <miguelmodd@gmail.com>
2025-05-31 16:38:53 -0400

    agregando dto

---
+++ Expensive.Application/Models/dto/CategoryDto.cs
@@ -0,0 +1,28 @@
+﻿using Expensive.Application.Response;
+using Expensive.Domain.Entities;
+
+namespace Expensive.Application.Models.dto;
+
+public class CategoryDto : BaseResponse
+{
+    public long CategorieId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+
+    public static explicit operator CategoryDto(Categories categories)
+    {
+        return new CategoryDto
+        {
+            CategorieId = categories.CategorieId,
+            Name = categories.Name,
+            Description = categories.Description,
+            CreatedAt = categories.CreatedAt,
+            CreatedBy = categories.CreatedBy,
+            UpdatedAt = categories.UpdatedAt,
+            UpdatedBy = categories.UpdatedBy,
+            IsActive = categories.IsActive,
+            IsDeleted = categories.IsDeleted,
+            RowGuid = categories.RowGuid
+        };
+    }
+}
