0e489363103420bb0a7e46d49b4ea514b208c270
MiguelMods <miguelmodd@gmail.com>
2025-05-31 14:11:46 -0400

    actualizando namespace para usar file scope

--- Expensive.Api/Requests/CategorieAddRequest.cs
+++ Expensive.Api/Requests/CategorieAddRequest.cs
@@ -1,4 +1,3 @@
-﻿namespace Expensive.Api.Requests
-{
-    public record CategorieAddRequest(string Name, string Description, string Operation = "Expensive");
-}
+﻿namespace Expensive.Api.Requests;
+
+public record CategorieAddRequest(string Name, string Description, string Operation = "Expensive");
--- Expensive.Api/Requests/CategorieUpdate.cs
+++ Expensive.Api/Requests/CategorieUpdate.cs
@@ -1,4 +1,3 @@
-﻿namespace Expensive.Api.Requests
-{
-    public record CategorieUpdateRequest(string RowGuid, string Name, string Description, string Operation = "Expensive");
-}
+﻿namespace Expensive.Api.Requests;
+
+public record CategorieUpdateRequest(string RowGuid, string Name, string Description, string Operation = "Expensive");
